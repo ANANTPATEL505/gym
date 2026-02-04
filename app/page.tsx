2209f8d950f0a072ef6cd14bd70d36e2f0fa9843
ANANT PATEL <anant3163@gmail.com>
2026-02-04 13:45:04 +0530

    fix vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import GymWebsite from "@/comps/GymWebsite";
-import Cafeshop from "@/comps/cafe-shop";
+import Cafeshop from "@/comps/Cafe-shop";
 
 export default function HomePage() {
   return <Cafeshop />;
